4af24276eba1496bcc30fbb488d0540d0d1bbb1a
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-05-14 05:21:45 +0700

    fully finalized the model

--- hospitalmgmt/appointment/models.py
+++ hospitalmgmt/appointment/models.py
@@ -11,15 +11,22 @@ class Appointment(models.Model):
     phoneContact = models.CharField("Phone Number", max_length=200, null=True, blank=True)
     address = models.CharField("Address", max_length=200, null=True, blank=True)
     appointmentDate = models.DateTimeField("Last Updated")
-    class StatusName(models.TextChoices):
-        OPEN = "O", gtlz("Open")
-        CLOSED = "C", gtlz("Closed")
-        INPROG = "I", gtlz("In Progress")
-        SUSPENDED = "P", gtlz("Suspended")
-    status = models.CharField("Appointment Status",
-                              max_length=1,
+    nextRoom = models.CharField("Next Room Appointed", max_length=200, null=True, blank=True)
+    class StatusName(models.IntegerChoices):
+        OPEN = 0, gtlz("Open")
+        CLOSED = 1, gtlz("Closed")
+        INPROG = 2, gtlz("In Progress")
+        SUSPENDED = 3, gtlz("Suspended")
+    # Query with get_FOO_display()
+    status = models.IntegerField("Appointment Status",
                               choices=StatusName.choices,
                               default=StatusName.OPEN)
+    genderMale = models.BooleanField("Gender is Male", default=True)
+    description = models.TextField("Extra Description and Notes",null=True, blank=True)
     # META DATA
     lastUpdatedDate = models.DateTimeField("Last Updated", auto_now=True)
     creationDate = models.DateTimeField("Creation", auto_now_add=True)
+    def __str__(self):
+        return str(self.id)
+    def get_absolute_url(self):
+        return reverse('app-detail', args=[str(self.id)])
