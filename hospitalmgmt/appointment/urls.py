c7ae93aa3771e54c7d89fd7c3a382a133220b2fb
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-05-14 05:22:03 +0700

    added the detail page url

--- hospitalmgmt/appointment/urls.py
+++ hospitalmgmt/appointment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('details/<int:pk>', views.index, name='app-detail'), #TODO: replace the vies.index to the appropriate
 ]
