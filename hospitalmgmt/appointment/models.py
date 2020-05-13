2d275b591c199fcf6c65326a3cc775cd99e8eae4
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-05-14 04:56:02 +0700

    added the basic model

--- hospitalmgmt/appointment/models.py
+++ hospitalmgmt/appointment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as gtlz
+from django.urls import reverse
 
-# Create your models here.
+
+class Appointment(models.Model):
+    caseType = models.CharField("AnimuName Kanji", max_length=200, default="")
+    phoneContact = models.CharField("Phone Number", max_length=200, null=True, blank=True)
+    address = models.CharField("Address", max_length=200, null=True, blank=True)
+    appointmentDate = models.DateTimeField("Last Updated")
+    lastUpdatedDate = models.DateTimeField("Last Updated", auto_now=True)
+    creationDate = models.DateTimeField("Creation", auto_now_add=True)
