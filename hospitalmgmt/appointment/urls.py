617ca4d3d18bf2efc988669912ff77a9c1e2751f
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-05-15 01:20:29 +0700

    completed the updating

--- hospitalmgmt/appointment/urls.py
+++ hospitalmgmt/appointment/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.GeneralView.as_view(), name='all'),
+    path('verbose', views.GeneralView.as_view(), name='verbose'),
     path('new', views.new_entry, name='new'),
     path('details/<int:pk>', views.DetailView.as_view(), name='app-detail'),
-    path('details/<int:pk>/submit', views.DetailView.as_view(), name='app-post-detail'),
+    path('details/<int:pk>/submit', views.update, name='app-post-detail'),
 ]
--- hospitalmgmt/appointment/views.py
+++ hospitalmgmt/appointment/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import generic
+from django.urls import reverse
+from datetime import datetime, date, time
+
 from .models import Appointment
 # Create your views here.
 
 def new_entry(request):
     return render(request, 'new.html', context=context)
 
-def updates(request, pk):
-    return render(request, 'new.html')
+def update(request, pk):
+    if request.method == "POST":
+        obj = Appointment.objects.get(id=pk)
+        obj.familieName = request.POST['familyname']
+        obj.surName = get_none(request.POST['surname'])
+        obj.nickName = get_none(request.POST['nickname'])
+        obj.caseType = request.POST['case']
+        obj.phoneContact = get_none(request.POST['phone'])
+        obj.address = get_none(request.POST['address'])
+        obj.appointmentDate = to_datetime(request.POST['date'], request.POST['time'])
+        obj.nextRoom = request.POST['room'] if request.POST['room'] and request.POST['room'] != "-" else None
+        obj.status = request.POST['stats']
+        obj.genderMale = request.POST['gender'] == "m"
+        obj.hidden = 'hide' in request.POST
+        obj.description = get_none(request.POST['desc'])
+        obj.save()
+    return HttpResponseRedirect(reverse('app-detail', args=[pk]) )
 
 
 class GeneralView(generic.ListView):
+    model = Appointment
+    paginate_by = 10
+    template_name = 'all.html'
+    def get_queryset(self):
+        return Appointment.objects.filter(hidden=False)
+
+class VerboseView(generic.ListView):
     model = Appointment
     paginate_by = 10
     template_name = 'all.html'
@@ -19,3 +44,11 @@ class GeneralView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Appointment
     template_name = 'detail.html'
+
+
+def get_none(text):
+    return text if text else None
+
+def to_datetime(date, time):
+    mixed = date+":"+time
+    return datetime.strptime(mixed,"%Y-%m-%d:%H:%M")
